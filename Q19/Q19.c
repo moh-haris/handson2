502cd14f64817f0b44f7f816f298d0d1a6149a93
Haris <113665185+moh-haris@users.noreply.github.com>
2025-11-08 13:05:50 +0530

    Refactor FIFO creation in Q19.c
    Simplified FIFO creation by directly returning the result of mknod.

--- Q19/Q19.c
+++ Q19/Q19.c
@@ -1,15 +1,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    const char *name_fifo = "fifo_call";
-    if (mknod(name_fifo, S_IFIFO | 0666, 0) == -1) {
-        perror("mknod");
-        exit(EXIT_FAILURE);
-    }
-    printf("FIFO created using mknod() system call: %s\n", name_fifo);
-    return 0;
+    return mknod("fifo1", S_IFIFO | 0666, 0);
 }
+
