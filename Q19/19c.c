4fd8ae749c503de181b87f5e2bdf2542c212a5ed
Haris <113665185+moh-haris@users.noreply.github.com>
2025-11-08 13:10:46 +0530

    Create 19c.c

---
+++ Q19/19c.c
@@ -0,0 +1,35 @@
+/*
+========================================================================================================
+Name : Q19
+Author : Mohammed Haris
+Description : Create a FIFO file by
+a. mknod command
+b. mkfifo command
+c. use strace command to find out, which command (mknod or mkfifo) is better.
+d. mknod system call
+e. mkfifo library function
+Date: 26 Oct, 2025.
+========================================================================================================
+*/
+
+#include <stdlib.h>
+
+int main() {
+    system("mknod test1 p");
+    system("mkfifo test2");
+    return 0;
+}
+
+
+
+/*
+============================================================================
+OUTPUT
+
+haris@Haris:~/handson2/Q19$ gcc Q19.c -o Q19
+haris@Haris:~/handson2/Q19$ strace ./Q19
+
+
+============================================================================
+*/
+
